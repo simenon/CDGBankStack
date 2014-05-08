756d6cb7aaae3c21914ee6fe82036d029e32d2fe
simenon <albert@simenon.nl>
2014-05-08 17:45:49 +0200

    Added some settings and fixed the control characters in itemlinks

--- CDGBankStack.lua
+++ CDGBankStack.lua
@@ -1,3 +1,4 @@
+local LAM = LibStub:GetLibrary("LibAddonMenu-1.0")
 local CDGBS = ZO_Object:Subclass()
 
 local CDGBankStack = {
@@ -5,11 +6,35 @@ local CDGBankStack = {
 		addonName = "CDGBankStack"
 	},
 	defaults = {
-		logToDefaultChat = false
+		logToDefaultChat = true,
+		logToCDGShowLoot = true,
 	}
 }
 
-function CDGBS:eventBankOpen(...)
+local CDGBS_SV = {}
+
+local function StripControlCharacters(s)
+	return string.gsub(s,"%^%a","")
+end
+
+local function InitializeLAMSettings()
+	local lamID = CDGBankStack.general.addonName .."LAM"
+	local panelID = LAM:CreateControlPanel(lamID, "CDG Bank Stack")
+	LAM:AddHeader(panelID, lamID.."Header".."GO", "General Options")
+	LAM:AddCheckbox(panelID, lamID.."CheckBox".."LogDefault", "Log to default chat", nil, function() return CDGBS_SV.logToDefaultChat end, function(value) CDGBS_SV.logToDefaultChat = value end,  false, nil)
+	LAM:AddCheckbox(panelID, lamID.."CheckBox".."LogToCDGShowLoot", "Log to CDG Show Loot", nil, function() return CDGBS_SV.logToCDGShowLoot end, function(value) CDGBS_SV.logToCDGShowLoot = value end,  false, nil)
+end
+
+local function logActionToChat(msg)
+	if CDGBS_SV.logToDefaultChat then
+		d(msg)
+	end
+	if CDGBS_SV.logToCDGShowLoot and CDGLibGui then
+		CDGLibGui.addMessage(msg)
+	end
+end
+
+function CDGBS:EVENT_OPEN_BANK(...)
 	local _, bankSlots = GetBagInfo(BAG_BANK)
 	local _, bagSlots = GetBagInfo(BAG_BACKPACK)
 
@@ -28,11 +53,8 @@ function CDGBS:eventBankOpen(...)
 				end
 				
 				if bankStack ~= bankMaxStack then
-					msg = "Banked " .. quantity.."/"..bagStack .. " " ..bagItemLink
-					d(msg)
-					if CDGLibGui ~= nil then
-						CDGLibGui.addMessage(msg)
-					end
+					logActionToChat("Banked " .. quantity.."/"..bagStack .. " " .. StripControlCharacters(bagItemLink))
+
 					CallSecureProtected("PickupInventoryItem",BAG_BACKPACK, bagSlot, bankMaxStack-bankStack)
 					CallSecureProtected("PlaceInTransfer")
 				end
@@ -42,6 +64,27 @@ function CDGBS:eventBankOpen(...)
 	end
 end
 
+function CDGBS:EVENT_ADD_ON_LOADED(eventCode, addOnName, ...)
+	if(addOnName == CDGBankStack.general.addonName) then
+		--
+		-- Initialize our saved variabeles, if needed
+		--
+		CDGBS_SV = ZO_SavedVars:New(CDGBankStack.general.addonName.."_SV", 1, nil, CDGBankStack.defaults)
+		--
+		-- Register our stuff in the addon settings
+		--
+		InitializeLAMSettings()
+		--
+		-- Register on all the other events we want to listen on
+		--
+		EVENT_MANAGER:RegisterForEvent(CDGBankStack.general.addonName, EVENT_OPEN_BANK, function(...) CDGBS:EVENT_OPEN_BANK(...) end)
+		--
+		-- A nice message to say the addon is loaded
+		--
+		logActionToChat("|cFF2222CrazyDutchGuy's|r Bank Stacker |c0066990.2|r Loaded")
+	end
+end
+
 function CDGBS_OnInitialized()
-	EVENT_MANAGER:RegisterForEvent("CDGBankStack", EVENT_OPEN_BANK, function(...) CDGBS:eventBankOpen(...) end)	
+	EVENT_MANAGER:RegisterForEvent(CDGBankStack.general.addonName, EVENT_ADD_ON_LOADED, function(...) CDGBS:EVENT_ADD_ON_LOADED(...) end )	
 end
